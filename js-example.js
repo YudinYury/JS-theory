afb155fe54daf2e25b9ab70ef3ec1d7f685f90c7
YudinYury <juri40prog@gmail.com>
2018-08-09 09:52:50 +0300

    update

--- js-example.js
+++ js-example.js
@@ -16,6 +16,26 @@ function sumArgs() {
 console.log(maxArgs(1, 3, 7)); // 7, аргументы переданы через запятую, без массива
 console.log(sumArgs(1, 3, 7)); // 11, аргументы переданы через запятую, без массива
 
+
+const context = { foo: "bar" };
+function returnFoo (param) {
+    if(param) {
+        return this.foo + '-' + param;
+    } else {
+        return this.foo;
+    }
+}
+
+// const bind = Function.prototype.call.bind(Function.prototype.bind);
+// const amazing = bind(returnFoo, context);
+
+const amazing = returnFoo.bind(context);
+console.log(amazing('bind'));  // => bar
+
+console.log(returnFoo.call(context, 'call'));  // => bar
+console.log(returnFoo.apply(context, ['apply']));  // => bar
+
+
 /*
 const barker = (state) => ({
     bark: () => console.log('Woof, I am ' + state.name)
