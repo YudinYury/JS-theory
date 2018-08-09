0e823802837a7e5e49447cc6d089c695e051ff75
YudinYury <juri40prog@gmail.com>
2018-08-09 12:03:35 +0300

    update

--- ts-example.ts
+++ ts-example.ts
@@ -3,7 +3,11 @@ interface Named {
 }
  
 let x: Named;
+let z = { name: "Alice"};
+console.log('z =', z);
 // выведенный для y тип — { name: string; location: string; }
 let y = { name: "Alice", location: "Seattle" };
 x = y;
-console.log(x);
+console.log('x =', x);
+z = y;
+console.log('z =', z);
